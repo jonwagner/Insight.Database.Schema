18ec21fdee8331b45998ec0f3967fe14aa8f4ee7
Jon Wagner <jonwagner@hotmail.com>
2014-03-10 08:56:37 -0400

    Updated build to support pre-release

--- Build.psake.ps1
+++ Build.psake.ps1
@@ -11,6 +11,7 @@ properties {
 
     $version = git describe --abbrev=0 --tags
     $changeset = (git log -1 $version --pretty=format:%H)
+	$assemblyversion = $version.Split('-', 2)[0]
 
     $outputDir = "$baseDir\Build\Output"
     $net40Path = "$baseDir\Insight.Database.Schema\bin\NET40"
@@ -32,8 +33,8 @@ function Replace-Version {
     )
 
     (Get-Content $Path) |
-		% { $_ -replace "\[assembly: AssemblyVersion\(`"(\d+\.?)*`"\)\]","[assembly: AssemblyVersion(`"$version`")]" } |
-		% { $_ -replace "\[assembly: AssemblyFileVersion\(`"(\d+\.?)*`"\)\]","[assembly: AssemblyFileVersion(`"$version`")]" } |
+		% { $_ -replace "\[assembly: AssemblyVersion\(`"(\d+\.?)*`"\)\]","[assembly: AssemblyVersion(`"$assemblyversion`")]" } |
+		% { $_ -replace "\[assembly: AssemblyFileVersion\(`"(\d+\.?)*`"\)\]","[assembly: AssemblyFileVersion(`"$assemblyversion`")]" } |
 		Set-Content $Path
 }
 
